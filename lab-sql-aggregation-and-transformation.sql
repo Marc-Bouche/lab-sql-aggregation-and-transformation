b87ab5121e48e008fbcef05c67d8a55719ca3873
Marc-Bouche <mbouch17@gmail.com>
2024-10-10 10:57:35 +0200

    Bonus question added

--- lab-sql-aggregation-and-transformation.sql
+++ lab-sql-aggregation-and-transformation.sql
@@ -18,7 +18,7 @@ LIMIT 10;
 
 
 #1.2. Express the average movie duration in hours and minutes. Don't use decimals.
-#Hint: Look for floor and round functions.
+	#Hint: Look for floor and round functions.
 #In Hours
 SELECT FLOOR(AVG(length) / 60) AS hours,
        AVG(length) % 60 AS minutes
@@ -30,7 +30,7 @@ FROM film;
 
 #2. You need to gain insights related to rental dates:
 #2.1 Calculate the number of days that the company has been operating.
-#Hint: To do this, use the rental table, and the DATEDIFF() function to subtract the earliest date in the rental_date column from the latest date.
+	#Hint: To do this, use the rental table, and the DATEDIFF() function to subtract the earliest date in the rental_date column from the latest date.
 
 #Identify first rental_date
 SELECT rental_date
@@ -48,18 +48,45 @@ FROM rental;
 
 
 #2.2 Retrieve rental information and add two additional columns to show the month and weekday of the rental. Return 20 rows of results.
-SELECT *, MONTH(rental_date) AS Month, DAY(rental_date) AS Day
+SELECT *, MONTH(rental_date) AS Month, 
+DAY(rental_date) AS Day
 FROM rental
 LIMIT 20;
 
 
 #2.3 Bonus: Retrieve rental information and add an additional column called DAY_TYPE with values 'weekend' or 'workday', depending on the day of the week.
-#Hint: use a conditional expression.
-#You need to ensure that customers can easily access information about the movie collection. To achieve this, retrieve the film titles and their rental duration. If any rental duration value is NULL, replace it with the string 'Not Available'. Sort the results of the film title in ascending order.
+	#Hint: use a conditional expression.
 
-#Please note that even if there are currently no null values in the rental duration column, the query should still be written to handle such cases in the future.
-#Hint: Look for the IFNULL() function.
-#Bonus: The marketing team for the movie rental company now needs to create a personalized email campaign for customers. To achieve this, you need to retrieve the concatenated first and last names of customers, along with the first 3 characters of their email address, so that you can address them by their first name and use their email address to send personalized recommendations. The results should be ordered by last name in ascending order to make it easier to use the data.
+
+SELECT *,
+    CASE 
+        WHEN WEEKDAY(rental_date) IN (0, 1, 2, 3, 4) THEN 'Workday'
+        WHEN WEEKDAY(rental_date) IN (5, 6) THEN 'Weekend'
+    END AS day_type
+FROM rental;
+
+#3.You need to ensure that customers can easily access information about the movie collection. To achieve this, retrieve the film titles and their rental duration. 
+		#If any rental duration value is NULL, replace it with the string 'Not Available'. Sort the results of the film title in ascending order.
+		#Please note that even if there are currently no null values in the rental duration column, the query should still be written to handle such cases in the future.
+		#Hint: Look for the IFNULL() function.
+
+SELECT title, 
+	IFNULL(rental_duration, 'Not Available') AS rental_duration
+FROM film
+ORDER BY title ASC;
+
+
+#4 Bonus: The marketing team for the movie rental company now needs to create a personalized email campaign for customers. 
+		#To achieve this, you need to retrieve the concatenated first and last names of customers, along with the first 3 characters of their email address, 
+		#so that you can address them by their first name and use their email address to send personalized recommendations. 
+		#The results should be ordered by last name in ascending order to make it easier to use the data.
+        
+SELECT 
+	CONCAT(first_name,last_name) as full_name,
+	SUBSTRING(email,1,3) AS Email_abreviation
+FROM customer
+ORDER BY last_name ASC;
+
 #-------------------------------------------------------------------------------------#
 
 
